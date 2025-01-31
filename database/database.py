cd479c17c00817338d4d219f0b9d2f7e05cdeb21
psamborski <psambek@gmail.com>
2025-01-31 18:42:46 +0100

    Changed DB type from SQLite to PostgreSQL

--- database/database.py
+++ database/database.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database/flat_rent_api.db"
+# Załaduj zmienne środowiskowe
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# Użyj zmiennej środowiskowej DATABASE_URL
+DATABASE_URL = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@db:5432/{os.getenv("DB_NAME")}'
+
+# Utwórz silnik SQLAlchemy
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
